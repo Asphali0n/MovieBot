1453063585012841d23c09acd1cc3620196cda77
Raphael <raphael@chelly.net>
2026-02-04 17:54:50 +0100

    tt

--- src/Movie.java
+++ src/Movie.java
@@ -8,11 +8,10 @@ public class Movie {
     private HashMap<String,Integer> seances;
 
     // Constructeurs
-    public Movie(String title, int price, String[] seances, int nb_places) {
+    public Movie(String title, int price, HashMap<String,Integer> seances, int nb_places) {
         this.title = title;
         this.price = price;
         this.seances = seances;
-        this.nb_places = nb_places;
     }
 
     // Getters
@@ -25,12 +24,8 @@ public class Movie {
         return this.price;
     }
 
-    public String[] getSeances(){
+    public HashMap<String,Integer> getSeances(){
         return this.seances;
-    }
-
-    public int nb_places getNb_places(){
-        return this.nb_places;
     }
 
 
@@ -38,6 +33,14 @@ public class Movie {
 
     public void setTitle(String new_title){
         this.title = new_title;
+    }
+
+    public void setPrice(int new_price){
+        this.price = new_price;
+    }
+
+    public void setSeances(HashMap<String,Integer> new_seances){
+        this.seances = new_seances;
     }
 
 }
--- src/User.java
+++ src/User.java
@@ -2,4 +2,8 @@ public class User {
 
     private String name;
     private int age;
+
+    // Getters
+
+    public String getName
 }
